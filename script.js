b43ffe2491d7593e9030245c3a3328787a156616
wanderssong <wanderssong545@gmail.com>
2024-08-13 14:38:39 -0400

    update , html e css

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.getElementById('unitsBtn').addEventListener('click', function(event) {
-    event.preventDefault(); // Evita comportamento padrão do link
-    const unitsSection = document.getElementById('unitsSection');
+document.getElementById('toggleUnitsBtn').addEventListener('click', function(event) {
+    event.preventDefault();
+    var unitsSection = document.getElementById('unitsSection');
     if (unitsSection.style.display === 'none' || unitsSection.style.display === '') {
-        unitsSection.style.display = 'block'; // Mostra as unidades
+        unitsSection.style.display = 'block';
     } else {
-        unitsSection.style.display = 'none'; // Esconde as unidades
+        unitsSection.style.display = 'none';
     }
 });
